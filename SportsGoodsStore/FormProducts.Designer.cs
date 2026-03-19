c37736abf823ae561ec6db2d42389eea0dc9fcb0
Бк <>
2026-03-19 11:29:05 +0500

    Создание формы FormLogin

---
+++ SportsGoodsStore/FormProducts.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SportsGoodsStore
+{
+    partial class FormProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            panelTop = new Panel();
+            dvgProducts = new DataGridView();
+            buttonLogin = new Button();
+            lblUserName = new Label();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dvgProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(lblUserName);
+            panelTop.Controls.Add(buttonLogin);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(10, 10);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(0, 0, 0, 10);
+            panelTop.Size = new Size(964, 40);
+            panelTop.TabIndex = 0;
+            // 
+            // dvgProducts
+            // 
+            dvgProducts.AllowUserToAddRows = false;
+            dvgProducts.AllowUserToDeleteRows = false;
+            dvgProducts.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dvgProducts.BackgroundColor = Color.FromArgb(248, 249, 250);
+            dvgProducts.BorderStyle = BorderStyle.None;
+            dvgProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dvgProducts.DefaultCellStyle = dataGridViewCellStyle1;
+            dvgProducts.Dock = DockStyle.Fill;
+            dvgProducts.Location = new Point(10, 50);
+            dvgProducts.MultiSelect = false;
+            dvgProducts.Name = "dvgProducts";
+            dvgProducts.ReadOnly = true;
+            dvgProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dvgProducts.Size = new Size(964, 601);
+            dvgProducts.TabIndex = 1;
+            // 
+            // buttonLogin
+            // 
+            buttonLogin.BackColor = Color.FromArgb(67, 97, 238);
+            buttonLogin.Dock = DockStyle.Right;
+            buttonLogin.FlatAppearance.BorderSize = 0;
+            buttonLogin.FlatStyle = FlatStyle.Flat;
+            buttonLogin.Location = new Point(814, 0);
+            buttonLogin.Name = "buttonLogin";
+            buttonLogin.Size = new Size(150, 30);
+            buttonLogin.TabIndex = 5;
+            buttonLogin.Text = "Войти";
+            buttonLogin.UseVisualStyleBackColor = false;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(769, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(45, 19);
+            lblUserName.TabIndex = 6;
+            lblUserName.Text = "label1";
+            lblUserName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // FormProducts
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(248, 249, 250);
+            ClientSize = new Size(984, 661);
+            Controls.Add(dvgProducts);
+            Controls.Add(panelTop);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FormProducts";
+            Padding = new Padding(10);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Список товаров";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dvgProducts).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private DataGridView dvgProducts;
+        private Label lblUserName;
+        private Button buttonLogin;
+    }
+}
